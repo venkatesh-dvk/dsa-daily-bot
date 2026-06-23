b046165ea1baa76e8159ec8b8852fc132e9e79cd
Venkatesh-dvk <devarakondavenkateshwarlu4@gmail.com>
2026-06-23 23:29:31 +0530

    Day 1 - Spring Boot setup

---
+++ src/main/java/com/dsa/bot/DsaDailyBotApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaDailyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaDailyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsa/bot/DsaDailyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaDailyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
